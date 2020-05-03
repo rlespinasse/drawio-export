0dfc624c627a73b7792d69adcfaf3f5cd68d8d87
rlespinasse <romain.lespinasse@gmail.com>
2020-05-03 20:03:46 +0200

    fix: clean link text from draw.io link format

--- drawio-export.sh
+++ drawio-export.sh
@@ -3,98 +3,188 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+link_separator='###'
+
 if [ -z "${DRAWIO_CLI:-}" ]; then
   echo >&2 "define DRAWIO_CLI as the path to draw.io desktop application"
   exit 1
 fi
 
-function export_pages() {
-  local path="$1"
-  local folder="$2"
-  local filename="$3"
-  local protected_filename="${filename// /-}"
-  local pagenum=0
-  local pagefileext="$FILEEXT"
-
-  if [ "$FILEEXT" == "adoc" ]; then
-    pagefileext="png"
-  fi
-
-  while read -r page; do
-    local protected_page="${page// /-}"
-    pagenum=$((pagenum + 1))
-    printf "++ export page %s : %s\n" "$pagenum" "$page"
-    printf "+++ generate %s file\n" "$pagefileext"
-    output_file="$folder/$FOLDER/$protected_filename-$protected_page.$pagefileext"
-
-    # shellcheck disable=SC2086
-    "$DRAWIO_CLI" \
-      --no-sandbox \
-      -x \
-      -f "$pagefileext" \
-      $CLI_OPTIONS \
-      -p "$pagenum" \
-      -o "$output_file" \
-      "$path" 2> >(
-        # Remove Election 9 deprecation warnings from error output
-        grep -v "allowRendererProcessReuse is deprecated" |
-          grep -v "DeprecationWarning: Passing functions"
-      )
-
-    if [ "$FILEEXT" == "adoc" ]; then
-      printf "+++ generate %s file\n" "$FILEEXT"
-
-      adoc_file="$folder/$FOLDER/$protected_filename-$protected_page.$FILEEXT"
-      {
-        echo "= ${filename} ${page}"
-        echo ""
-        echo "image::${protected_filename}-${protected_page}.${pagefileext}[${page}]"
-        echo ""
-      } >"$adoc_file"
-      echo "$adoc_file"
-
-      printf "+++ include links in %s file\n" "$FILEEXT"
-
-      xmldata=$({
-        printf "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x00"
-        sgrep '"name=\"'"$page"'\""..">".."<"' "$path" | sed 's/<$//;s/.*">//' | base64 -d -w 0
-      } | (gzip -dc 2>/dev/null || true) | sed 's@+@ @g;s@%@\\x@g')
-
-      local linknum=0
-      while read -r linkdata; do
-        linknum=$((linknum + 1))
-        local text="${linkdata//###*/}"
-        local link="${linkdata//*###/}"
-        printf "link %s : %s\n" "$linknum" "$link"
-        printf "link text %s : %s\n" "$linknum" "$text"
-        printf "* %s[%s]\n" "$link" "$text" >>"$adoc_file"
-      done < <(
-        printf "%b" "$xmldata" |
-          sgrep '"<UserObject"..">"' |
-          sed 's/<UserObject/\n/g' |
-          sed 's/.*label="//g;s/" link="/###/g;s/".*//;/^$/d'
-      )
-    fi
-  done < <(sgrep '"name=\"".."\""' "$path" | sed 's/^name="//;s/"name="/\n/g;s/"$//')
-}
-
-function export_file() {
-  while read -r current_path; do
-    echo "+ export file : $current_path"
-    current_folder=$(dirname "$current_path")
-    current_file=$(basename "$current_path")
-    current_filename="${current_file%.*}"
-
-    echo "++ prepare export folder : $current_folder/$FOLDER"
-    mkdir -p "$current_folder/$FOLDER"
-
-    export_pages "$current_path" "$current_folder" "$current_filename"
-  done < <(find . -name "*.drawio" | sort)
-}
-
 command -v sgrep >/dev/null 2>&1 || {
   echo >&2 "I require sgrep but it's not installed.  Aborting."
   exit 1
+}
+
+export_drawio_files() {
+  local folder
+  local file
+  local filename
+
+  while read -r path; do
+    echo "+ export file : $path"
+    folder=$(dirname "$path")
+    file=$(basename "$path")
+    filename="${file%.*}"
+
+    echo "++ prepare export folder : $folder/$OUTPUT_FOLDER"
+    mkdir -p "$folder/$OUTPUT_FOLDER"
+
+    export_drawio_pages "$path" "$folder" "$filename"
+  done < <(find . -name "*.drawio" | sort)
+}
+
+export_drawio_pages() {
+  local path="$1"
+  local folder="$2"
+  local filename="$3"
+
+  local pagenum=0
+  while read -r page; do
+    pagenum=$((pagenum + 1))
+    export_drawio_page "$pagenum" "$page" "$filename" "$path" "$folder"
+  done < <(sgrep '"name=\"".."\""' "$path" | sed 's/^name="//;s/"name="/\n/g;s/"$//')
+}
+
+export_drawio_page() {
+  local pagenum="$1"
+  local page="$2"
+  local filename="$3"
+  local path="$4"
+  local folder="$5"
+
+  local export_type="$EXPORT_TYPE"
+  if [ "$EXPORT_TYPE" == "adoc" ]; then
+    export_type="png"
+  fi
+
+  local output_filename
+  output_filename="$folder/$OUTPUT_FOLDER/${filename// /-}-${page// /-}"
+
+  printf "++ export page %s : %s\n" "$pagenum" "$page"
+  printf "+++ generate %s file\n" "$export_type"
+  export_diagram_file "$export_type" "$path" "$pagenum" "$output_filename"
+
+  if [ "$EXPORT_TYPE" == "adoc" ]; then
+    echo "+++ generate adoc file"
+    create_asciidoc_page "$path" "$filename" "$page" "$output_filename"
+
+    echo "+++ include links in adoc file"
+    include_links_in_asciidoc_page "$path" "$page" "$output_filename"
+  fi
+}
+
+export_diagram_file() {
+  local export_type="$1"
+  local path="$2"
+  local pagenum="$3"
+  local output_filename="$4"
+
+  # shellcheck disable=SC2086
+  "$DRAWIO_CLI" \
+    --no-sandbox \
+    -x \
+    -f "$export_type" \
+    $CLI_OPTIONS \
+    -p "$pagenum" \
+    -o "$output_filename.$export_type" \
+    "$path" 2> >(
+      # Remove Election 9 deprecation warnings from error output
+      grep -v "allowRendererProcessReuse is deprecated" |
+        grep -v "DeprecationWarning: Passing functions"
+    )
+}
+
+create_asciidoc_page() {
+  local path="$1"
+  local filename="$2"
+  local page="$3"
+  local output_filename="$4"
+
+  {
+    echo "= ${filename} ${page}"
+    echo ""
+    echo "image::${output_filename}.png[${page}]"
+    echo ""
+  } >"$output_filename.adoc"
+  echo "$output_filename.adoc"
+}
+
+include_links_in_asciidoc_page() {
+  local path="$1"
+  local page="$2"
+  local output_filename="$3"
+
+  local rawxml
+  rawxml=$(get_rawxml_diagram_page "$path" "$page")
+
+  local linknum=0
+  while read -r linkdata; do
+    linknum=$((linknum + 1))
+    include_link_in_asciidoc_page "$linknum" "$linkdata" "$output_filename"
+  done < <(
+    printf "%b" "$rawxml" |
+      sgrep '"<UserObject"..">"' |
+      sed 's/<UserObject/\n/g' |
+      sed 's/.*label="//g;s/" link="/'"$link_separator"'/g;s/".*//;/^$/d'
+  )
+}
+
+get_rawxml_diagram_page() {
+  local path="$1"
+  local page="$2"
+
+  {
+    printf "\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\x00"
+    sgrep '"name=\"'"$page"'\""..">".."<"' "$path" | sed 's/<$//;s/.*">//' | base64 -d -w 0
+  } | (gzip -dc 2>/dev/null || true) | sed 's@+@ @g;s@%@\\x@g'
+}
+
+include_link_in_asciidoc_page() {
+  local linknum="$1"
+  local linkdata="$2"
+  local output_filename="$3"
+
+  local link
+  link=$(
+    echo "$linkdata" |
+      # Isolate the link
+      sed 's/.*'"$link_separator"'//' |
+      # Clean <br>
+      sed 's/&lt;br&gt;\?/ /g' |
+      # Clean &nbsp;
+      sed 's/&amp;nbsp;/ /g' |
+      # Clean <div>text</div>
+      sed 's/&lt;div&gt;/ /g;s/&lt;\/div&gt;\?/ /g' |
+      # Trim space
+      sed 's/\s\+/ /g;s/^\s//;s/\s$//'
+  )
+
+  local text
+  text=$(
+    echo "$linkdata" |
+      # Isolate the link text
+      sed 's/'"$link_separator"'.*//' |
+      # Clean <br>
+      sed 's/&lt;br&gt;\?/ /g' |
+      # Clean &nbsp;
+      sed 's/&amp;nbsp;/ /g' |
+      # Clean <div>text</div>
+      sed 's/&lt;div&gt;/ /g;s/&lt;\/div&gt;\?/ /g' |
+      # Trim space
+      sed 's/\s\+/ /g;s/^\s//;s/\s$//'
+  )
+
+  printf "link text %s : %s\n" "$linknum" "$text"
+  if [ "$link" == "$text" ]; then
+    echo >&2 "WARNING: link is the same as the link text"
+  elif [ "$link" == "" ]; then
+    echo >&2 "WARNING: link is empty"
+  elif [ "${link//,*/}" == "data:page/id" ]; then
+    echo >&2 "WARNING: link between pages is not currently supported"
+  else
+    printf "link %s : %s\n" "$linknum" "$link"
+    printf "* %s&[%s]\n" "$link" "$text" >>"$output_filename.adoc"
+  fi
 }
 
 args="${*}"
@@ -108,8 +198,8 @@ DEFAULT_FOLDER="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # shellcheck disable=SC1090
 source "$DEFAULT_FOLDER/drawio-default.env"
 
-FILEEXT=${DRAWIO_EXPORT_FILEEXT:-${DEFAULT_DRAWIO_EXPORT_FILEEXT}}
+EXPORT_TYPE=${DRAWIO_EXPORT_FILEEXT:-${DEFAULT_DRAWIO_EXPORT_FILEEXT}}
 CLI_OPTIONS=${DRAWIO_EXPORT_CLI_OPTIONS:-${DEFAULT_DRAWIO_EXPORT_CLI_OPTIONS}}
-FOLDER=${DRAWIO_EXPORT_FOLDER:-${DEFAULT_DRAWIO_EXPORT_FOLDER}}
+OUTPUT_FOLDER=${DRAWIO_EXPORT_FOLDER:-${DEFAULT_DRAWIO_EXPORT_FOLDER}}
 
-export_file
+export_drawio_files
