94339604f1cbf1d96ab74126570216afb021f9ed
rlespinasse <romain.lespinasse@gmail.com>
2022-10-01 15:48:11 +0200

    feat: support drawio app > 16

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM rust:buster as drawio-exporter-installer
 
 RUN cargo install --version 1.2.0 drawio-exporter
 
-FROM rlespinasse/drawio-desktop-headless:1.5.0
+FROM rlespinasse/drawio-desktop-headless:v1.7.0
 
 WORKDIR /opt/drawio-exporter
 COPY --from=drawio-exporter-installer /usr/local/cargo/bin/drawio-exporter .
@@ -10,7 +10,7 @@ COPY scripts/* ./
 
 # disable timeout capabilities since it's a batch
 ENV DRAWIO_DESKTOP_COMMAND_TIMEOUT 0
-ENV DRAWIO_DESKTOP_RUNNER_COMMAND_LINE "/opt/drawio-exporter/runner-no-security-warnings.sh"
+ENV DRAWIO_DESKTOP_RUNNER_COMMAND_LINE "/opt/drawio-exporter/runner.sh"
 ENV DRAWIO_DESKTOP_EXECUTABLE_PATH /opt/drawio-exporter/drawio-exporter
 
 WORKDIR /data
--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-filter_electron_security_warnings() {
-  while read -r line; do
-    echo "$line" | grep -v "is deprecated and will be changing"
-  done
-}
-
 if [ "${ELECTRON_DISABLE_SECURITY_WARNINGS}" == "true" ]; then
-  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 2> >(filter_electron_security_warnings)
+  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 3>&1 >&2 2>&3 3>&- |
+    grep -v "Failed to connect to socket" |
+    grep -v "Could not parse server address" |
+    grep -v "Floss manager not present" |
+    grep -v "Exiting GPU process" |
+    grep -v "called with multiple threads" |
+    grep -v "extension not supported" |
+    grep -v "Failed to send GpuControl.CreateCommandBuffer"
 else
   "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 2>&1
 fi
