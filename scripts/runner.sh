fe89cdb3f4584ebfd35b847681295b3cad1843ed
rlespinasse <romain.lespinasse@gmail.com>
2023-06-29 21:25:22 +0200

    fix: use previous election security warning filtering

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 if [ "${ELECTRON_DISABLE_SECURITY_WARNINGS}" == "true" ]; then
-  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 3>&1 >&2 2>&3 3>&- |
+  "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 2>&1 |
     grep -v "Failed to connect to socket" |
     grep -v "Could not parse server address" |
     grep -v "Floss manager not present" |
     grep -v "Exiting GPU process" |
     grep -v "called with multiple threads" |
     grep -v "extension not supported" |
-    grep -v "Failed to send GpuControl.CreateCommandBuffer"
+    grep -v "Failed to send GpuControl.CreateCommandBuffer" |
+    grep -v "Init observer found at shutdown"
 else
   "$DRAWIO_DESKTOP_EXECUTABLE_PATH" --drawio-desktop-headless "$@" 2>&1
 fi
--- tests/base.bats
+++ tests/base.bats
@@ -11,10 +11,10 @@ docker_test() {
   shift
   run docker container run -t $docker_opts -w /data -v $(pwd)/${data_folder:-}:/data ${DOCKER_IMAGE} "$@"
 
-  echo "$output" > "tests/output/$output_file.log"
+  echo "$output" | tee "tests/output/$output_file.log" | sed 's#\[.*:.*/.*\..*:.*:.*\(.*\)\] ##' >"tests/output/$output_file-comp.log"
 
   [ "$status" -eq $status ]
   if [ -f "tests/expected/$output_file.log" ]; then
-    [ "$(diff --strip-trailing-cr <(echo "$output") "tests/expected/$output_file.log")" = "" ]
+    [ "$(diff --strip-trailing-cr "tests/output/$output_file-comp.log" "tests/expected/$output_file.log")" = "" ]
   fi
 }
